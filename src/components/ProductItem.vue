46e6dbaf4ab6222bce2bd8649bb0809d01d6f080
tjamesmac <tjamesmac@gmail.com>
2019-10-03 20:10:11 +0100

    Add productItem

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -2,25 +2,6 @@
   <div>
     <h2>{{ info.name }}</h2>
     <p v-html="info.description"></p>
-    <ul v-if="info.variants">
-      <li v-for="item in info.variants" :key="item.id">
-        <input
-          type="radio"
-          v-bind:id="item.name"
-          :name="'group' + info.name"
-          v-bind:value="item.name"
-        />
-        <label v-bind:for="'group' + info.name" class="pl-2">
-          <span class="font-weight-bold">{{ item.name }}</span> :
-          <span class="font-weight-bold" style="text-decoration: line-through;">
-            £{{ item.price / 100 }}
-          </span>
-          <span class="text-danger font-weight-bold">
-            £{{ (item.price / 2 / 100).toFixed(2) }}
-          </span>
-        </label>
-      </li>
-    </ul>
     <button
       class="btn btn-primary btn-green"
       aria-label="Find out more about this product"
@@ -41,18 +22,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
 ul {
   list-style-type: none;
   padding: 0;
-}
-// li {
-//   display: inline-block;
-//   margin: 0 10px;
-// }
-a {
-  color: #42b983;
 }
 </style>
